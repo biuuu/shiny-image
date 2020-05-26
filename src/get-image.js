44c6264fe3c53f52aff19bd113876f11cac99ca1
umisuna <10892119+biuuu@users.noreply.github.com>
2020-05-26 10:05:12 +0800

    Add some images

--- src/get-image.js
+++ src/get-image.js
@@ -1,27 +1,8 @@
 const info = require('../image-info.json')
+const imageList = require('../image-list.json')
 const { downloadImage } = require('./utils')
 const sharp = require('sharp')
 const fs = require('fs-extra')
-
-const imageList = [
-'images/ui/common/parts_buttons.json_image',
-'images/ui/start_and_common/parts.json_image',
-'images/ui/my_page/parts.json_image',
-'images/ui/present_box/parts.json_image',
-'images/ui/mission/parts.json_image',
-'images/ui/idol_portal/parts.json_image',
-'images/ui/fes/parts.json_image',
-'images/ui/produce_top/parts.json_image',
-'images/ui/beginner_mission/parts.json_image',
-'images/ui/produce_action/parts_wing.json_image',
-'images/ui/produce_common/parts_header.json_image',
-'images/ui/produce_common/parts_pyramid.json_image',
-'images/ui/produce_trend/parts.json_image',
-'images/ui/produce_action/gradation/parts.json_image',
-'images/ui/produce_action/parts.json_image',
-'images/ui/produce_event/parts_event.json_image',
-'images/ui/produce_ability/parts.json_image',
-'images/ui/produce_block/parts.json_image']
 
 const extract = async (name, frames) => {
   for (let key in frames) {
