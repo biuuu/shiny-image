e3f149c5eaaf10b8f0490165c0ea44cbf50c5013
mio <10892119+biuuu@users.noreply.github.com>
2020-12-07 10:04:35 +0800

    update

--- src/compress.js
+++ src/compress.js
@@ -1,14 +1,33 @@
 const imagemin = require('imagemin')
 const imageminPngquant = require('imagemin-pngquant')
+const glob = require('glob')
 
 const start = async () => {
 	console.log('compressing...')
 	try {
-		await imagemin(['./temp/*.png'], {
+		const files = glob.sync('./temp/*.png')
+		const list = []
+		const uncomp = []
+		files.forEach(file => {
+			if (!file.includes('produce_common_parts_header')) {
+				list.push(file)
+			} else {
+				uncomp.push(file)
+			}
+		})
+		await imagemin(list, {
 			destination: './dist/image/',
 			plugins: [
 				imageminPngquant({
-					quality: [0.7, 0.9]
+					quality: [0.5, 0.6]
+				})
+			]
+		})
+		await imagemin(uncomp, {
+			destination: './dist/image/',
+			plugins: [
+				imageminPngquant({
+					quality: [0.95, 1]
 				})
 			]
 		})
